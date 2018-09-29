b4223a6947afb34361c0b08f307966a285b7a287
Jean A. Sena <jadsena@hotmail.com>
2018-09-29 15:12:58 -0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Fiap01.Data;
+using Fiap01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private PerguntasContext _context;
+        public HomeController(PerguntasContext perguntasContext)
+        {
+            _context = perguntasContext;
+        }
+        public IActionResult Index()
+        {
+            //ViewBag.Nome = "Jean";
+            //ViewData["NomeDoAluno"] = $"Outro Nome {DateTime.Now}";
+            //var pergunta = new Pergunta()
+            //{
+            //    Id=1,
+            //    Descricao="Que pergunta?",
+            //    Autor="Jean"
+            //};
+
+            return View(_context.Perguntas.ToList());
+        }
+        public IActionResult Ajuda()
+        {
+            return View();
+        }
+        public IActionResult Sobre()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Pergunta pergunta)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Perguntas.Add(pergunta);
+                await _context.SaveChangesAsync();
+                var p = pergunta;
+            }
+            return View();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,48 @@
+﻿using Fiap01.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap01
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.AspNetCore.NewDb;Trusted_Connection=True;ConnectRetryCount=0";
+            services.AddDbContext<PerguntasContext>(db=> db.UseSqlServer(connection));
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            app.UseMvc(routes =>
+            {
+                //routes.MapRoute(
+                //    name: "autor",
+                //    template: "autor/{nome}",
+                //    defaults: new { controller = "Autor", action = "Index" });
+                //routes.MapRoute(
+                //    name: "autoresdoano",
+                //    template: "{ano:int}/autor",
+                //    defaults: new { controller = "Autor", action = "ListaDosAutoresDoAno" });
+                //routes.MapRoute(
+                //    name: "topicosdacategoria",
+                //    template: "{categoria}/{topico}",
+                //    defaults: new { controller = "Topicos", action = "Index" });
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
